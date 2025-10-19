1a974eb523c2fe7ef6a0bf04c0ddbad55fba0060
biancasbublitz <bsbublitz@inf.ufsm.br>
2025-10-19 11:54:42 -0300

    sensor de distancia ultrassonico

---
+++ distancia/distancia.ino
@@ -0,0 +1,21 @@
+int sensorPin = 21; //Pino D0 do sensor conectado na porta 21 do Arduino
+int readingSensor;
+int count = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sensorPin, INPUT);
+}
+
+void loop() {
+  readingSensor = digitalRead(sensorPin);
+
+  if ( readingSensor == 1) {
+    count++; 
+
+    Serial.print("Numero de deteccoes: ");
+    Serial.println(count);
+
+    delay(500);
+  }
+}
